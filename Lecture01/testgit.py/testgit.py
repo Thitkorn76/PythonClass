c76836a5e940d0bf91735a8d7cfb070155348bde
Thitikorn76 <thitikorn.euro2@gmail.com>
2023-07-10 10:45:17 +0700

    Draw the first Flowchart

--- Lecture01/testgit.py/testgit.py
+++ Lecture01/testgit.py/testgit.py
@@ -1,1 +1,14 @@
-int(input("Enter Number : "))
+Number1 = int(input("Enter number :"))
+Number2 = int(input("Enter number :"))
+
+
+
+
+
+
+
+
+# weight = int(input("Enter your weiht :"))
+# Height = int(input("Enter your heiht :"))/100
+# print("BMI = ,Weight//(Height**2)")
+
